6b13e747d73b0f60d544415cbec55ce337f9b8e2
Qutäy <peterszabototh@gmail.com>
2020-10-13 20:28:42 +0200

    Init

---
+++ src/main/java/com/viahunters/onepage/OnepageApplication.java
@@ -0,0 +1,13 @@
+package com.viahunters.onepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepageApplication.class, args);
+	}
+
+}
